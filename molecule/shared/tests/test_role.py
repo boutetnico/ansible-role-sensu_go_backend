9177e4853fb7e064f90ef2d49eea715fb482ebe6
Nicolas Boutet <amd3002@gmail.com>
2021-01-12 12:11:15 +0100

    Add http test on backend health endpoint

--- molecule/shared/tests/test_role.py
+++ molecule/shared/tests/test_role.py
@@ -2,6 +2,8 @@ import pytest
 
 import os
 
+import requests
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -15,3 +17,12 @@ def test_container(host, name):
     container = host.docker(name)
     assert container.name == name
     assert container.is_running
+
+
+@pytest.mark.parametrize('endpoint', [
+  ('http://127.0.0.1:8080/health'),
+])
+def test_sensu_backend_is_reachable(endpoint):
+    response = requests.get(endpoint)
+    data = response.json()
+    assert data['ClusterHealth'][0]['Healthy']
