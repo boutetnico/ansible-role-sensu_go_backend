79d4eb31a910c28f9992f2849b53039d35a8b880
Nicolas Boutet <amd3002@gmail.com>
2023-04-30 10:01:33 +0200

    Adapt CI workflow for molecule 5

--- molecule/shared/tests/test_role.py
+++ molecule/shared/tests/test_role.py
@@ -7,22 +7,29 @@ import requests
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('sensu-backend'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("sensu-backend"),
+    ],
+)
 def test_container(host, name):
     container = host.docker(name)
     assert container.name == name
     assert container.is_running
 
 
-@pytest.mark.parametrize('endpoint', [
-  ('http://127.0.0.1:8080/health'),
-])
+@pytest.mark.parametrize(
+    "endpoint",
+    [
+        ("http://127.0.0.1:8080/health"),
+    ],
+)
 def test_sensu_backend_is_reachable(endpoint):
     response = requests.get(endpoint)
     data = response.json()
-    assert data['ClusterHealth'][0]['Healthy']
+    assert data["ClusterHealth"][0]["Healthy"]
