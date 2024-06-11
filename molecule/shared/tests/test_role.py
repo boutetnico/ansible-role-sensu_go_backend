cc5ed24c030789e81354fb363234874c8e39e4d5
Nicolas Boutet <amd3002@gmail.com>
2024-06-11 09:08:29 +0200

    Fix tests with molecule 6

--- molecule/shared/tests/test_role.py
+++ molecule/shared/tests/test_role.py
@@ -1,14 +1,6 @@
 import pytest
 
-import os
-
 import requests
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ["MOLECULE_INVENTORY_FILE"]
-).get_hosts("all")
 
 
 @pytest.mark.parametrize(
